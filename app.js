837ef6113aa11b6177c91fb0342047575033b76b
5lsolutionsmagbanua@gmail.com <5lsolutionsmagbanua@gmail.com>
2026-05-22 16:51:03 +0800

    First Commit
    STATIC FORM

---
+++ app.js
@@ -0,0 +1,50 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var { CheckConnection } = require("./repository/dbconnection");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dashboardRouter = require("./routes/dashboard");
+var inventoryRouter = require("./routes/inventory");
+var shipmentsRouter = require("./routes/shipments");
+var warehouseRouter = require("./routes/warehouse");
+
+var app = express();
+// console.log("working", CheckConnection())
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/dashboard", dashboardRouter);
+app.use("/inventory", inventoryRouter);
+app.use("/shipments", shipmentsRouter);
+app.use("/warehouse", warehouseRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
