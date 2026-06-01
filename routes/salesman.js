75496966042ef781eec7275c58637650f0cd5d23
5lsolutionsmagbanua@gmail.com <5lsolutionsmagbanua@gmail.com>
2026-06-01 16:31:07 +0800

    Add New Feature
    Create Shipment Forms

--- routes/salesman.js
+++ routes/salesman.js
@@ -1,9 +1,10 @@
+
 var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("users", { title: "Users Page" });
+  res.render("salesman", { title: "Salesman Page" });
 });
 
 module.exports = router;
